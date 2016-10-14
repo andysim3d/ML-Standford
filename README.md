b0634d7ec270859ff30887a791e5347e966d586b
pengfei zhang <pzhang11@stevens.edu>
2016-10-14 16:45:14 -0400

    Finish opts assignment

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Stanford Machine Learning course
 This repo is build for share homework/assignment about Standford ML course.
+
 [refer](https://www.coursera.org/learn/machine-learning/) 
--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -9,11 +9,12 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
+J = ((X * theta - y)' * (X * theta - y)) / (2 * m);
 
 
 
